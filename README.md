561787d0e747b6e5739c43fc0eb694554389e882
stunwin <steve.unwin@gmail.com>
2025-05-09 17:06:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# lilguys.nvim
+
+Sometimes you just need to add a lil guy to the line!
+
+This plugin allows you to easily insert a symbol like `|>` at the start or cursor location of a line in supported filetypes.
+
+I made it because typing `|>` on my keyboard is moderately inconvenient, but also because I've never made a real plugin before.
+
+## ✨ Features
+
+- Toggle lil guy (`|>`) at the start of the line (supports multi-line selections!)
+- Insert lil guy at cursor
+- Filetype-sensitive activation
+- Configurable keymaps and symbol
+
+## 📦 Installation
+
+Requres Neovim `0.7.0`
+
+### Using `lazy.nvim`
+
+```lua
+{
+  'stunwin/lilguys.nvim',
+  config = function()
+    require('lilguys').setup()
+  end
+}
+```
+
+### Using `packer.nvim`
+
+```lua
+use {
+  'stunwin/lilguys.nvim',
+  config = function()
+    require('lilguys').setup()
+  end
+}
+```
+
+## ⚙️ Configuration
+
+- Default keybind to toggle a lil guy at the start of the line is `<leader>.`
+- Default keybind to insert a lil guy at your cursor is  `<leader>,`
+
+```lua
+require('lilguys').setup({
+  keybind = '<leader>.',  -- toggles a lil guy in at the front of the line. Respects whitespace.
+  insert_keybind = '<leader>,',  -- adds a lil guy and a space right before your curor
+  symbol = '|>',  -- want a different lil guy? put him here!
+  filetypes = { 'gleam', 'elm', 'elixir' },  -- your lil guy probably only works in certain files. make sure they're here
+  auto_insert = true, -- if you use the main keybind on a new, empty line, this will kick you straight into insert mode... or not!
+})
+```
+
+## todo
+
+- [ ] Set per-filetype lil guys
+- [ ] Three-way toggle between start of line, at-cursor, and off states
+- [ ] record some lil gifs of product in action
+- [ ] Support for massive concurrency and cloud deployment
+- [ ] finish this and probably get back to real work
+
+## credits
+
+I got the idea for this from [obsidian.nvim](https://github.com/epwalsh/obsidian.nvim)'s checkboxes
+
+I would have no idea what I'm doing without [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
---
+++ lua/lilguys/init.lua
@@ -0,0 +1,91 @@
+local M = {}
+
+-- Default config
+local config = {
+  keybind = '<leader>.',
+  insert_keybind = '<leader>,',
+  symbol = '|>',
+  filetypes = { 'gleam', 'elm', 'elixr' },
+  auto_insert = true,
+}
+
+-- Setup function for user config
+M.setup = function(user_config)
+  config = vim.tbl_deep_extend('force', config, user_config or {})
+  vim.keymap.set({ 'n', 'v' }, config.keybind, M.insert_at_start, { desc = 'Add lil guy pipe at start of line' })
+  vim.keymap.set({ 'n' }, config.insert_keybind, M.insert_at_cursor, { desc = 'Add lil guy pipe at cursor' })
+end
+
+M.check_filetype = function()
+  local current_ft = vim.bo.filetype
+  if #config.filetypes > 0 and not vim.tbl_contains(config.filetypes, current_ft) then
+    vim.notify("no lil guys for file type '" .. current_ft .. "'", vim.log.levels.DEBUG)
+    return false
+  else
+    return true
+  end
+end
+
+M.insert_at_start = function()
+  -- Check filetype
+  if not M.check_filetype() then
+    return
+  end
+
+  -- Determine the selected line range
+  local start_line = vim.fn.line '.'
+  local end_line = vim.fn.line 'v'
+
+  -- flip orientation if selection is going up
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  for line_num = start_line, end_line do
+    guy_added = false
+    local line = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, false)[1]
+
+    if line ~= nil then
+      local indent, rest = line:match '^(%s*)(.*)'
+
+      local escaped = vim.pesc(config.symbol)
+      if rest:find('^' .. escaped .. '%s*') then
+        rest = rest:gsub('^' .. escaped .. '%s*', '')
+        line = indent .. rest
+      else
+        line = indent .. config.symbol .. ' ' .. rest
+        if rest == '' then
+          guy_added = true
+        end
+      end
+
+      vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, false, { line })
+    end
+  end
+
+  -- jump into insert if we just threw a guy into a blank line
+  if vim.fn.mode() == 'n' and config.auto_insert and guy_added then
+    vim.api.nvim_input '$i'
+  end
+
+  if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
+    vim.api.nvim_input '<Esc>'
+  end
+end
+
+M.insert_at_cursor = function()
+  local escaped = vim.pesc(config.symbol)
+
+  if not M.check_filetype() then
+    return
+  end
+
+  if vim.fn.mode() == 'n' then
+    vim.api.nvim_input('i' .. escaped .. '<Space><Esc>l')
+    -- if config.auto_insert then
+    --   vim.api.nvim_input 'i'
+    -- end
+  end
+end
+
+return M
